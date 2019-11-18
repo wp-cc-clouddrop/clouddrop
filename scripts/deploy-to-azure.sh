f1168a4fe2b6b539939846115f65d588ed2d0cfb
M Aiman Ismail <MuhammadAimanBin.Ismail@haw-hamburg.de>
2019-11-18 23:10:02 +0100

    remove scripts folder

--- scripts/deploy-to-azure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# TODO: run Dockerfile to build and run unit-tests
-
-# zero exit value means success
-# dummy non-zero exit value for now to signal error
-echo "DEPLOYING TO AZURE"
-exit 0
